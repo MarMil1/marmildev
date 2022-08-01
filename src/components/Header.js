b520cacc3424a1ea5a2f97f6aa17d3d9a0b95459
MarMil1 <milosavljevic.marko@outlook.com>
2022-08-01 14:49:16 -0500

    Adding react scroll dependency

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import '../styles/header.css'
+import { Link } from "react-scroll";
 
 export const Header = () => {
     useEffect(() => {
@@ -17,40 +18,91 @@ export const Header = () => {
 
   return (
     <header>
-        <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+        <nav 
+            className="navbar is-transparent is-fixed-top" 
+             role="navigation" 
+             aria-label="main navigation"
+        >
             <div className="navbar-brand">
-                <a className="navbar-item" href="#hero">
-                    <img src="" width="112" height="28" alt="logo"/>
-                </a>
+                <Link
+                    activeClass=""
+                    to="hero"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                >
+                    <img src="" 
+                         width="112" 
+                         height="28" 
+                         alt="logo"
+                    />
+                </Link>
             
-                <button className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                <button 
+                    className="navbar-burger" 
+                    data-target="navMenu" 
+                    aria-label="menu" 
+                    aria-expanded="false"
+                >
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </button>
             </div>
 
-            <div className="navbar-menu is-shadowless has-text-centered" id="navMenu">
+            <div 
+                className="navbar-menu is-shadowless has-text-centered" 
+                id="navMenu"
+            >
                 <div className="navbar-end">
                     <div className="navbar-item">
-                        <a className="has-text-info" href="#about">
-                            About
-                        </a>
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration= {500}
+                    >
+                        About
+                    </Link>
                     </div>
                     <div className="navbar-item">
-                        <a className="has-text-info" href="#projects">
+                        <Link 
+                            activeClass="active"
+                            to="projects"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration= {500}
+                        >
                             Projects
-                        </a>
+                        </Link>
                     </div>
                     <div className="navbar-item">
-                        <a className="has-text-info" href="#work">
+                        <Link 
+                            activeClass="active"
+                            to="work"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration= {500}
+                        >
                             Work
-                        </a>
+                        </Link>
                     </div>
                     <div className="navbar-item">
-                        <a className="contact-btn button is-info is-rounded" href="#contact">
+                        <Link 
+                            className="contact-btn button is-info is-rounded"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration= {500}
+                        >
                             Contact Me
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
